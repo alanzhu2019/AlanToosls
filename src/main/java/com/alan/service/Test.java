3202a39a3a75b51eea7d23c3c861ac92292b2a8f
alan <1401516947@qq.com>
2022-02-12 22:21:49 +0800

    Stringbuffer测试文件

--- src/main/java/com/alan/service/Test.java
+++ src/main/java/com/alan/service/Test.java
@@ -1,4 +1,12 @@
 package com.alan.service;
 
 public class Test {
+
+    public static void main(String[] args) {
+        StringBuffer alan = new StringBuffer("alan");
+
+        StringBuffer stringBuffer = new StringBuffer(100);
+
+        StringBuilder stringBuilder = new StringBuilder();
+    }
 }
