3f4fe52b067ac968fc6c1156772a92f6e059bad1
alan <1401516947@qq.com>
2022-02-13 21:43:31 +0800

    Math练习

---
+++ src/main/java/com/alan/Test/MathTest.java
@@ -0,0 +1,20 @@
+package com.alan.Test;
+
+public class MathTest {
+
+    public static void main(String[] args) {
+
+        int abs = Math.abs(-9);
+        System.out.println(abs);
+
+        double pow = Math.pow(2, 4);
+        System.out.println(pow);
+
+
+        int floor = (int)Math.floor(Math.random() * 6 + 2);
+        System.out.println(floor);
+
+
+
+    }
+}
